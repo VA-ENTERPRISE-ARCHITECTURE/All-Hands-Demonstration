81a8d0cd9e15795d11d84cd46c73dd34132385df
Black <james.black@va.gov>
2018-05-09 15:23:43 -0400

    This is the initial commit. This is just a demonstration project.

---
+++ AllHandsDemonstration/src/Main/Main.java
@@ -0,0 +1,16 @@
+package Main;
+
+/*
+ * This is just a test class. 
+ * The main function will be executed when the Java
+ * program is executed either through the client or
+ * through the compiled JAR file 
+ * The purpose of this class is to show the functionality
+ * of Git and GitHub. 
+ */
+
+public class Main {
+	public static void main(String[] args) {
+
+	}	
+}
