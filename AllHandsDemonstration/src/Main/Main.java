8f92ce3aa64299934ca0b4ccc0506d0caa4d8a27
VACOBenjaM2 <VACOBenjaM2@VACOLTBENJAM1.dva.va.gov>
2018-05-11 09:50:09 -0400

    changes made

--- AllHandsDemonstration/src/Main/Main.java
+++ AllHandsDemonstration/src/Main/Main.java
@@ -1,20 +1,63 @@
 package Main;
 
+import java.util.Random;
+
 /*
+
  * This is just a test class. 
+
  * The main function will be executed when the Java
+
  * program is executed either through the client or
+
  * through the compiled JAR file 
+
  * The purpose of this class is to show the functionality
+
  * of Git and GitHub. 
+
  */
 
-public class Main {
-	public static void main(String[] args) {
 
-	}	
-	
-	public static void test() {
-		
-	}
+public class Main {
+
+        
+
+        static private String print = "Hello World"; 
+
+        
+
+        public static void main(String[] args) {
+
+                doWork(); 
+
+        }       
+
+        
+
+        private static void doWork() {
+
+                System.out.println("Starting work...");
+
+                Random randomNum = new Random(); 
+
+                int times = randomNum.nextInt(25) + 1; 
+
+                printLine(times); 
+
+        }
+
+        
+
+        private static void printLine(int times) {
+
+                for(int x = 0; x < times; x++) {
+
+                        System.out.println(print + ": " + times);
+
+                }
+
+        }
+
 }
+
